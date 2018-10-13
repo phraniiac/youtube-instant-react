ec19405a23b0db9175dc234c1c67fa8aab88385b
Pranav <prsharma@HW15808.local>
2018-10-13 22:33:18 +0530

    Updates

---
+++ precache-manifest.91275253962365b081a633c38e9fe42a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1875124e12a465b17c0b",
+    "url": "/static/js/main.1875124e.chunk.js"
+  },
+  {
+    "revision": "12a1d37524d193ed10a0",
+    "url": "/static/js/1.12a1d375.chunk.js"
+  },
+  {
+    "revision": "1875124e12a465b17c0b",
+    "url": "/static/css/main.449f003a.chunk.css"
+  },
+  {
+    "revision": "3ac3bd4fc707d528909a188f2ff40ce9",
+    "url": "/index.html"
+  }
+];
